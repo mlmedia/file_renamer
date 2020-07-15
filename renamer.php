0ede8edd1900e95b1379a6691e092598ee53632e
mlmedia <mike@millionlightsmedia.com>
2020-07-15 14:27:38 -0400

    - formatting clean up

--- renamer.php
+++ renamer.php
@@ -1,33 +1,27 @@
 <?php
 
 /* get the requested action */
-$get_action	= filter_input( INPUT_GET, 'action', FILTER_SANITIZE_STRING );
+$get_actio n= filter_input( INPUT_GET, 'action', FILTER_SANITIZE_STRING );
 
 /* if the query string has action=db */
-if ( $get_action == 'db' )
-{
+if ( $get_action == 'db' ) {
 	/* connect to the DB */
-	try
-	{
-		$database	= 'my_database';
-		$username 	= 'mysql_user';
-		$password 	= 'myPassword123';
-		$conn 		= new PDO( 'mysql:host=localhost;dbname=' . $database, $username, $password );
+	try {
+		$database= 'my_database';
+		$username = 'mysql_user';
+		$password = 'myPassword123';
+		$conn = new PDO( 'mysql:host=localhost;dbname=' . $database, $username, $password );
 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch( PDOException $e )
-	{
+	} catch( PDOException $e ) {
 		echo 'ERROR: ' . $e->getMessage( );
 		$conn = false;
 	}
 
-	if ( $conn )
-	{
+	if ( $conn ) {
 		/* get the records */
 		$data = $conn->query( 'SELECT * FROM files_table' );
 
-		foreach( $data as $row )
-		{
+		foreach( $data as $row ) {
 			$new_uri = rename_file_path( $row[ 'uri' ], true );
 			$new_fn = rename_file_path( $row[ 'filename' ] );
 			$stmt = $conn->prepare( 'UPDATE files_table SET uri = :uri, filename = :filename WHERE id = :id' );
@@ -35,7 +29,7 @@ if ( $get_action == 'db' )
 				array(
 					':id' => $row[ 'id' ],
 					':uri' => $new_uri,
-					':filename'	=> $new_fn
+					':filename'=> $new_fn
 				)
 			);
 			echo 'URI -> ' . $new_uri . "\n<br />";
@@ -49,8 +43,7 @@ if ( $get_action == 'db' )
 	$dhandle = opendir( $dir );
 	$old_files = array( );
 
-	if ( $dhandle )
-	{
+	if ( $dhandle ) {
 		while (false !== ( $fname = readdir( $dhandle ) ) ) {
 			if ( ( $fname != '.' ) && ( $fname != '..' ) && ! is_dir( './' . $fname ) ){
 				$old_files[ ] = $fname;
@@ -59,8 +52,7 @@ if ( $get_action == 'db' )
 		closedir( $dhandle );
 	}
 
-	foreach( $old_files as $file )
-	{
+	foreach( $old_files as $file ) {
 		$new_name = rename_file_path( $file );
 		rename( "./" . $file, "./" . $new_name );
 		echo 'FILE -> ' . $new_name . "\n<br />";
@@ -72,23 +64,20 @@ if ( $get_action == 'db' )
  */
 function rename_file_path( $input = null, $full_path = false )
 {
-	if ( $input )
-	{
-		$ext		= strtolower( pathinfo( $input, PATHINFO_EXTENSION ) );
-		$full_file 	= $full_path ? substr( strrchr( $input, '/' ), 1 ) : $input;
-		$filename 	= str_replace( '.' . $ext, '', $full_file );
-		$dirname	= str_replace( $filename . '.' . $ext, '', $input );
-		$new_name 	= str_replace( '\'', '', strtolower( $filename ) ); /* apostrophes to spaces - lone exception to an underscore since "that_s_" looks bad */
-		$new_name 	= str_replace( ' ', '_', strtolower( $new_name ) );
-		$new_name 	= preg_replace( '/[^a-z0-9\_]+/i', '_', $new_name ); /* convert everything except case-insensitive alpha-numeric to underscores */
-		for ( $i=0;$i<3;$i++ )
-		{
-			$new_name 	= str_replace( '__', '_', $new_name );
+	if ( $input ) {
+		$ext = strtolower( pathinfo( $input, PATHINFO_EXTENSION ) );
+		$full_file = $full_path ? substr( strrchr( $input, '/' ), 1 ) : $input;
+		$filename = str_replace( '.' . $ext, '', $full_file );
+		$dirname = str_replace( $filename . '.' . $ext, '', $input );
+		$new_name = str_replace( '\'', '', strtolower( $filename ) ); /* apostrophes to spaces - lone exception to an underscore since "that_s_" looks bad */
+		$new_name = str_replace( ' ', '_', strtolower( $new_name ) );
+		$new_name = preg_replace( '/[^a-z0-9\_]+/i', '_', $new_name ); /* convert everything except case-insensitive alpha-numeric to underscores */
+		for ( $i=0;$i<3;$i++ ) {
+			$new_name = str_replace( '__', '_', $new_name );
 		}
-		if ( $ext && $new_name )
-		{
-			$output 	= $dirname . $new_name . '.' . $ext;
-			$output 	= str_replace( '_.', '.', $output ); /* remove last underscore before extension, if there is one */
+		if ( $ext && $new_name ) {
+			$output = $dirname . $new_name . '.' . $ext;
+			$output = str_replace( '_.', '.', $output ); /* remove last underscore before extension, if there is one */
 			return $output;
 		}
 	}
